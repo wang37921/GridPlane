a24c8a9eea840a7a8df197a0f044ecfdf56233d4
Wang Cong <wang37921@gmail.com>
2019-10-17 13:15:47 +0800

    first commit

---
+++ Assets/GridPlaneSample/Editor/GridPlaneEditor.cs
@@ -0,0 +1,197 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+[CustomEditor(typeof(GridPlane))]
+public class GridPlaneEditor : Editor
+{
+    bool _isEditing = false;
+    GridUsage _editingUsages;
+    HashSet<int> _brushingGrids = new HashSet<int>();
+
+    private void OnEnable()
+    {
+        var gridPlane = target as GridPlane;
+        if (gridPlane.Grids == null && (gridPlane.Row > 0 || gridPlane.Column > 0))
+            gridPlane.UpdateDimension(gridPlane.Row, gridPlane.Column);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        var gridPlane = target as GridPlane;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        _isEditing = GUILayout.Toggle(_isEditing, EditorGUIUtility.IconContent("EditCollider"), GUI.skin.GetStyle("Button"), GUILayout.Width(35), GUILayout.Height(25));
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+        if (_isEditing)
+        {
+            GUILayout.BeginHorizontal();
+
+            var hasPlant = GUILayout.Toggle(_editingUsages.HasFlag(GridUsage.Plant), "作物", GUI.skin.button);
+            if (hasPlant)
+                _editingUsages |= GridUsage.Plant;
+            else
+                _editingUsages &= ~GridUsage.Plant;
+
+            var hasBuilding = GUILayout.Toggle(_editingUsages.HasFlag(GridUsage.Building), "建筑", GUI.skin.button);
+            if (hasBuilding)
+                _editingUsages |= GridUsage.Building;
+            else
+                _editingUsages &= ~GridUsage.Building;
+
+            var hasFurniture = GUILayout.Toggle(_editingUsages.HasFlag(GridUsage.Furniture), "家具", GUI.skin.button);
+            if (hasFurniture)
+                _editingUsages |= GridUsage.Furniture;
+            else
+                _editingUsages &= ~GridUsage.Furniture;
+
+            GUILayout.EndHorizontal();
+        }
+
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Dimension");
+        var row = EditorGUILayout.IntField(gridPlane.Row, GUILayout.Width(60));
+        GUILayout.Label("x", GUILayout.Width(15));
+        var col = EditorGUILayout.IntField(gridPlane.Column, GUILayout.Width(60));
+        GUILayout.EndHorizontal();
+        if (row != gridPlane.Row || col != gridPlane.Column)
+        {
+            Undo.RecordObject(gridPlane, "Update Grid Dimension");
+            gridPlane.UpdateDimension(row, col);
+        }
+
+        serializedObject.Update();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Grid Size");
+        var gridWidth = serializedObject.FindProperty("_gridSize.x");
+        gridWidth.floatValue = EditorGUILayout.IntField((int)gridWidth.floatValue, GUILayout.Width(60));
+        GUILayout.Label("x", GUILayout.Width(15));
+        var gridHeight = serializedObject.FindProperty("_gridSize.y");
+        gridHeight.floatValue = EditorGUILayout.IntField((int)gridHeight.floatValue, GUILayout.Width(60));
+        GUILayout.EndHorizontal();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    bool _isDraging;
+    Vector2 _beginPos;
+    Vector2 _endPos;
+
+    protected virtual void OnSceneGUI()
+    {
+        var gridPlane = target as GridPlane;
+        if (_isEditing)
+        {
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+            if (Event.current.button == 0)
+            {
+                if (Event.current.type == EventType.MouseDown)
+                    _beginPos = Event.current.mousePosition;
+                else if (Event.current.type == EventType.MouseDrag)
+                {
+                    _endPos = Event.current.mousePosition;
+                    _isDraging = true;
+                }
+                else if (Event.current.type == EventType.MouseUp)
+                {
+                    if (_isDraging)
+                    {
+                        _isDraging = false;
+
+                        foreach (var gridIndex in _brushingGrids)
+                        {
+                            Undo.RecordObject(gridPlane, "Grid Usage");
+                            gridPlane.Grids[gridIndex].UsageFlag = _editingUsages;
+                        }
+                        _brushingGrids.Clear();
+                    }
+                    else
+                    {
+                        var clickRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                        var gridIndex = gridPlane.GetGrid(clickRay);
+                        if (gridIndex != -1)
+                        {
+                            Undo.RecordObject(gridPlane, "Grid Usage");
+                            gridPlane.Grids[gridIndex].UsageFlag = _editingUsages;
+                            SceneView.currentDrawingSceneView.Repaint();
+                        }
+                    }
+                }
+            }
+            if (_isDraging)
+            {
+                Handles.BeginGUI();
+                var bodyColor = Color.gray;
+                bodyColor.a = 0.5f;
+
+                var dis = _endPos - _beginPos;
+                var dragRectInGUI = new Rect() { size = new Vector2(Mathf.Abs(dis.x), Mathf.Abs(dis.y)), center = Vector2.Lerp(_beginPos, _endPos, 0.5f) };
+
+                EditorGUI.DrawRect(dragRectInGUI, bodyColor);
+                Handles.EndGUI();
+                SceneView.currentDrawingSceneView.Repaint();
+
+                for (var gridIndex = 0; gridIndex < gridPlane.Grids.Length; ++gridIndex)
+                {
+                    var gridCenterInWorld = gridPlane.GetGridCenter(gridIndex);
+                    var gridCenterInGUI = HandleUtility.WorldToGUIPoint(gridCenterInWorld);
+                    if (dragRectInGUI.Contains(gridCenterInGUI))
+                    {
+                        var gridHalfSize = gridPlane.GridSize / 2.0f;
+                        Vector3[] verts = new Vector3[]
+                        {
+                            gridCenterInWorld - gridPlane.transform.right * gridHalfSize.x - gridPlane.transform.forward * gridHalfSize.y,
+                            gridCenterInWorld - gridPlane.transform.right * gridHalfSize.x + gridPlane.transform.forward * gridHalfSize.y,
+                            gridCenterInWorld + gridPlane.transform.right * gridHalfSize.x + gridPlane.transform.forward * gridHalfSize.y,
+                            gridCenterInWorld + gridPlane.transform.right * gridHalfSize.x - gridPlane.transform.forward * gridHalfSize.y,
+                        };
+                        var rectColor = Color.green;
+                        rectColor.a = 0.2f;
+                        var outlineColor = Color.blue;
+                        outlineColor.a = 0.5f;
+                        Handles.DrawSolidRectangleWithOutline(verts, rectColor, outlineColor);
+                        _brushingGrids.Add(gridIndex);
+                    }
+                }
+            }
+        }
+    }
+
+    [DrawGizmo(GizmoType.NotInSelectionHierarchy | GizmoType.Active)]
+    public static void DrawGizmos(GridPlane gridPlane, GizmoType gizmoType)
+    {
+        Gizmos.color = Color.black;
+        for (var row = 0; row <= gridPlane.Row; ++row)
+            Gizmos.DrawLine(gridPlane.RowBeginPosition(row), gridPlane.RowEndPosition(row));
+        for (var col = 0; col <= gridPlane.Column; ++col)
+            Gizmos.DrawLine(gridPlane.ColumnBeginPosition(col), gridPlane.ColumnEndPosition(col));
+        Gizmos.color = Color.white;
+
+        for (var gridIndex = 0; gridIndex != gridPlane.Grids.Length; ++gridIndex)
+        {
+            var grid = gridPlane.Grids[gridIndex];
+            var gridCenter = gridPlane.GetGridCenter(gridIndex);
+            if (grid != null)
+            {
+                if (grid.UsageFlag.HasFlag(GridUsage.Plant))
+                    Gizmos.DrawIcon(gridCenter - gridPlane.transform.right * 0.3f, "plant");
+                if (grid.UsageFlag.HasFlag(GridUsage.Furniture))
+                    Gizmos.DrawIcon(gridCenter, "furniture");
+                if (grid.UsageFlag.HasFlag(GridUsage.Building))
+                    Gizmos.DrawIcon(gridCenter + gridPlane.transform.right * 0.3f, "building");
+            }
+            else
+            {
+                Gizmos.DrawIcon(gridCenter - gridPlane.transform.right * 0.3f, "plant");
+                Gizmos.DrawIcon(gridCenter, "furniture");
+                Gizmos.DrawIcon(gridCenter + gridPlane.transform.right * 0.3f, "building");
+            }
+        }
+    }
+}
---
+++ Assets/GridPlaneSample/Scripts/GridPlane.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+
+[System.Flags]
+public enum GridUsage
+{
+    None = 0,
+    Furniture = 1,
+    Building = 2,
+    Plant = 4,
+}
+
+[System.Serializable]
+public class GridData
+{
+    public int X;
+    public int Y;
+    public GridUsage UsageFlag;
+}
+
+public class GridPlane : MonoBehaviour
+{
+    [SerializeField]
+    int _row = 1;
+    public int Row => _row;
+    [SerializeField]
+    int _column = 1;
+    public int Column => _column;
+    [SerializeField]
+    Vector2 _gridSize = Vector2.one;
+    public Vector2 GridSize => _gridSize;
+
+    // NOTE: Unity does not serialize multidimensional array!
+    public GridData[] Grids;
+
+    public static int Dimension2Index(int row, int col, int rowIndex, int colIndex)
+    {
+        if (rowIndex >= row || colIndex >= col)
+            return -1;
+        else
+            return rowIndex * col + colIndex;
+    }
+
+    public static bool Index2Dimension(int row, int column, int index, out int rowIndex, out int columnIndex)
+    {
+        rowIndex = index / column;
+        columnIndex = index % column;
+        return rowIndex < row && columnIndex < column;
+    }
+
+    public void UpdateDimension(int row, int col)
+    {
+        var grids = new GridData[row * col];
+        for (var rowIndex = 0; rowIndex < row; ++rowIndex)
+        {
+            for (var colIndex = 0; colIndex < col; ++colIndex)
+            {
+                var elementIndex = Dimension2Index(row, col, rowIndex, colIndex);
+                var oldElementIndex = Dimension2Index(_row, _column, rowIndex, colIndex);
+                if (oldElementIndex != -1 && Grids?.Length> oldElementIndex)
+                    grids[elementIndex] = Grids[oldElementIndex];
+                else
+                    grids[elementIndex] = new GridData { X = row, Y = col, UsageFlag = GridUsage.Plant|GridUsage.Furniture|GridUsage.Building };
+            }
+        }
+        Grids = grids;
+        _row = row;
+        _column = col;
+    }
+
+    public Vector3 RowBeginPosition(int row)
+    {
+        return transform.position + transform.forward * row * GridSize.y;
+    }
+
+    public Vector3 RowEndPosition(int row)
+    {
+        return RowBeginPosition(row) + transform.right * Column * GridSize.x;
+    }
+
+    public Vector3 ColumnBeginPosition(int col)
+    {
+        return transform.position + transform.right * col * GridSize.x;
+    }
+
+    public Vector3 ColumnEndPosition(int col)
+    {
+        return ColumnBeginPosition(col) + transform.forward * Row * GridSize.y;
+    }
+
+    public Vector3 GetGridCenter(int row, int col)
+    {
+        var localPos = new Vector3((col + 0.5f) * GridSize.x, 0.0f, (row + 0.5f) * GridSize.y);
+        return transform.TransformPoint(localPos);
+    }
+
+    public Vector3 GetGridCenter(int gridIndex)
+    {
+        if (Index2Dimension(Row, Column, gridIndex, out int rowIndex, out int columnIndex))
+        {
+            var localPos = new Vector3((columnIndex + 0.5f) * GridSize.x, 0.0f, (rowIndex + 0.5f) * GridSize.y);
+            return transform.TransformPoint(localPos);
+        }
+        else
+            return transform.position;
+    }
+
+    public bool GetGrid(Vector3 worldPos, out int row, out int col)
+    {
+        var localPos = transform.InverseTransformPoint(worldPos);
+        row = Mathf.FloorToInt(localPos.y / _gridSize.y);
+        col = Mathf.FloorToInt(localPos.x / _gridSize.x);
+        return row >=0 && row < _row && col >= 0 && col < _column;
+    }
+
+    public int GetGrid(Vector3 worldPos)
+    {
+        var localPos = transform.InverseTransformPoint(worldPos);
+        var rowIndex = Mathf.FloorToInt(localPos.z / _gridSize.y);
+        var colIndex = Mathf.FloorToInt(localPos.x / _gridSize.x);
+        var gridIndex = Dimension2Index(Row, Column, rowIndex, colIndex);
+        return gridIndex < Grids.Length ? gridIndex : -1;
+    }
+
+    public int GetGrid(Ray ray)
+    {
+        var plane = new Plane(transform.up, transform.position);
+        if (plane.Raycast(ray, out float enter))
+        {
+            var point = ray.GetPoint(enter);
+            return GetGrid(point);
+        }
+        else
+            return -1;
+    }
+
+    public bool GetGrid(Ray ray, out int row, out int col)
+    {
+        var plane = new Plane(transform.up, transform.position);
+        if (plane.Raycast(ray, out float enter))
+        {
+            var point = ray.GetPoint(enter);
+            return GetGrid(point, out row, out col);
+        }
+        row = 0; col = 0;
+        return false;
+    }
+}
